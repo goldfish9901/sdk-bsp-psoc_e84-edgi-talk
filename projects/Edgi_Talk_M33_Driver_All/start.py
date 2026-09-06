553e0c7a268db7f3c2f5c741bc9d8dfeed03b6c1
goldfish9901 <745727212@qq.com>
2026-09-06 09:47:43 +0800

    chore(Edgi_Talk): 添加 OpenOCD 烧录脚本与 VS Code 工作区配置

---
+++ projects/Edgi_Talk_M33_Driver_All/start.py
@@ -0,0 +1,76 @@
+import pathlib
+import sys
+import logging
+import subprocess
+import pydantic_cli
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+
+class OpenOcdCmdLineConfig:
+
+    def __init__(
+        self,
+        openOcdInstallDir: pathlib.Path,
+        projectMainDir: pathlib.Path = None,
+        scripts: list[pathlib.Path] = None,
+        cfgFiles: list[pathlib.Path] = None,
+        commands: list[str] = None,
+    ):
+        self.openOcdInstallDir = openOcdInstallDir
+        self.projectMainDir = projectMainDir or pathlib.Path(__file__).parent
+        self.cmdOcd = openOcdInstallDir / "bin" / "openocd.exe"
+        self.defaultScriptPath = openOcdInstallDir / "scripts"
+        self.scripts: list[pathlib.Path] = scripts or [
+            openOcdInstallDir / "scripts",
+            openOcdInstallDir / "flm" / "cypress" / "cat4",
+        ]
+        self.cfgFiles: list[pathlib.Path] = cfgFiles or [
+            self.defaultScriptPath / "interface" / "kitprog3.cfg",
+            self.defaultScriptPath / "target" / "infineon" / "pse84xgxs2.cfg",
+        ]
+        hex = pathlib.Path(__file__).parent / "build" / "rtthread.hex"
+        hex_str = str(hex.relative_to(self.projectMainDir).as_posix())
+        self.commands: list[str] = commands or [
+            "set QSPI_FLASHLOADER ../flm/cypress/cat4/PSE84_SMIF.FLM",
+            "transport select swd",
+            "set ENABLE_ACQUIRE 0",
+            "; ".join(
+                [
+                    "init",
+                    "reset init",
+                    f"flash write_image erase {hex_str}",
+                    "reset run",
+                    "exit",
+                ]
+            ),
+        ]
+
+    def build_args(self):
+        args = [str(self.cmdOcd)]
+        for script in self.scripts:
+            args.append("-s")
+            if not script.exists():
+                raise FileNotFoundError(f"script {script} not found")
+            args.append(str(script.as_posix()))
+        for cfg in self.cfgFiles:
+            args.append("-f")
+            args.append(str(cfg.relative_to(self.defaultScriptPath).as_posix()))
+        for cmd in self.commands:
+            args.append("-c")
+            args.append(cmd)
+        return args
+
+class Entry(pydantic_cli.Cmd):
+    cwd: pathlib.Path = pathlib.Path(__file__).parent
+    def run(self):
+        cfg = OpenOcdCmdLineConfig(
+            pathlib.Path("E:\\sdk\\openocd-5.19.0.4782-windows\\openocd")
+        )
+        cmd = cfg.build_args()
+        logging.info(f"cmd: {cmd}")
+        subprocess.run(cmd, check=True)
+
+if __name__ == "__main__":
+    pydantic_cli.run_and_exit(Entry)
